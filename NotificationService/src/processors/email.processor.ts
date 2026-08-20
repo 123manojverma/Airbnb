914d2e604192b64afe4fcf7cee9352b8c5cdab08
Manoj Verma <123manojverma2005@gmail.com>
2026-08-21 00:11:08 +0530

    Added email sending through hbs and nodemailer

--- NotificationService/src/processors/email.processor.ts
+++ NotificationService/src/processors/email.processor.ts
@@ -3,6 +3,9 @@ import { NotificationDto } from "../dto/notification.dto";
 import { MAILER_QUEUE } from "../queues/maller.queue";
 import { getRedisConnObject } from "../config/redis.config";
 import { MAILER_PAYLOAD } from "../producers/email.producer";
+import { renderMailTemplate } from "../templates/templates.handler";
+import { sendEmail } from "../services/mailer.service";
+import logger from "../config/logger.config";
 
 export const setupMailerWorker=()=>{
     const emailProcessor=new Worker<NotificationDto>(
@@ -15,6 +18,12 @@ export const setupMailerWorker=()=>{
             // call the service layer from here
             const payload=job.data;
             console.log(`Processing eamil for: ${JSON.stringify(payload)}`);
+
+            const emailContent=await renderMailTemplate(payload.templateId,payload.params);
+
+            await sendEmail(payload.to,payload.subject,emailContent)
+
+            logger.info(`Email sent to ${payload.id} with subject ${payload.subject}`)
         }, //Process function
         { 
             connection:getRedisConnObject()
