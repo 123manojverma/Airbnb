71b29af4f8d7b0d7b25a1436a2e7cbfb6a13454f
Manoj Verma <123manojverma2005@gmail.com>
2026-09-12 21:49:37 +0530

    Add request validators for role management and update role router

--- AuthInGo/middlewares/validator.go
+++ AuthInGo/middlewares/validator.go
@@ -13,22 +13,22 @@ func UserCreateRequestValidator(next http.Handler) http.Handler {
 		var payload dto.CreateUserRequestDTO // Define the type of payload you expect
 
 		// Read and decode the JSON body into the payload
-		if err:=utils.ReadJsonBody(r,&payload); err!=nil{
-			utils.WriteJsonErrorResponse(w,http.StatusBadRequest,"Invalid request body",err)
-			return 
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
 		}
 
 		//  Validate the payload using the Validator instance
-		if err:=utils.Validator.Struct(payload); err!=nil{
-			utils.WriteJsonErrorResponse(w,http.StatusBadRequest,"Validation failed",err)
-			return 
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
 		}
 
-		req_context:=r.Context()
+		req_context := r.Context()
 
-		ctx:=context.WithValue(req_context,"payload",payload)
+		ctx := context.WithValue(req_context, "payload", payload)
 
-		next.ServeHTTP(w,r.WithContext(ctx)) //Call the next handler in the chain
+		next.ServeHTTP(w, r.WithContext(ctx)) //Call the next handler in the chain
 	})
 }
 
@@ -37,23 +37,111 @@ func UserLoginRequestValidator(next http.Handler) http.Handler {
 		var payload dto.LoginUserRequestDTO // Define the type of payload you expect
 
 		// Read and decode the JSON body into the payload
-		if err:=utils.ReadJsonBody(r,&payload); err!=nil{
-			utils.WriteJsonErrorResponse(w,http.StatusBadRequest,"Invalid request body",err)
-			return 
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
 		}
 
 		//  Validate the payload using the Validator instance
-		if err:=utils.Validator.Struct(payload); err!=nil{
-			utils.WriteJsonErrorResponse(w,http.StatusBadRequest,"Validation failed",err)
-			return 
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
 		}
 
-		fmt.Println("Payload received for login:",payload)
+		fmt.Println("Payload received for login:", payload)
 
-		req_context:=r.Context()
+		req_context := r.Context()
 
-		ctx:=context.WithValue(req_context,"payload",payload)
+		ctx := context.WithValue(req_context, "payload", payload)
 
-		next.ServeHTTP(w,r.WithContext(ctx)) //Call the next handler in the chain
+		next.ServeHTTP(w, r.WithContext(ctx)) //Call the next handler in the chain
+	})
+}
+
+func CreateRoleRequestValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var payload dto.CreateRoleRequestDTO
+
+		// Read and decode the JSON body into the payload
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
+		}
+
+		// Validate the payload using the Validator instance
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "payload", payload)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func UpdateRoleRequestValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var payload dto.UpdateRoleRequestDTO
+
+		// Read and decode the JSON body into the payload
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
+		}
+
+		// Validate the payload using the Validator instance
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "payload", payload)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func AssignPermissionRequestValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var payload dto.AssignPermissionRequestDTO
+
+		// Read and decode the JSON body into the payload
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
+		}
+
+		// Validate the payload using the Validator instance
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "payload", payload)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func RemovePermissionRequestValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var payload dto.RemovePermissionRequestDTO
+
+		// Read and decode the JSON body into the payload
+		if err := utils.ReadJsonBody(r, &payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Invalid request body", err)
+			return
+		}
+
+		// Validate the payload using the Validator instance
+		if err := utils.Validator.Struct(payload); err != nil {
+			utils.WriteJsonErrorResponse(w, http.StatusBadRequest, "Validation failed", err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "payload", payload)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
--- AuthInGo/router/role_router.go
+++ AuthInGo/router/role_router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"AuthInGo/controllers"
+	"AuthInGo/middlewares"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -20,4 +21,15 @@ func (rr *RoleRouter) Register(r chi.Router){
 	// Role CRUD operations
 	r.Get("/roles/{id}", rr.RoleController.GetRoleById)
 	r.Get("/roles", rr.RoleController.GetAllRoles)
+	r.With(middlewares.CreateRoleRequestValidator).Post("/roles", rr.RoleController.CreateRole)
+	r.With(middlewares.UpdateRoleRequestValidator).Put("/roles/{id}", rr.RoleController.UpdateRole)
+	r.Delete("/roles/{id}", rr.RoleController.DeleteRole)
+
+	// Role permissions operations
+	r.Get("/roles/{id}/permissions", rr.RoleController.GetRolePermissions)
+	r.With(middlewares.AssignPermissionRequestValidator).Post("/roles/{id}/permissions", rr.RoleController.AssignPermissionToRole)
+	r.With(middlewares.RemovePermissionRequestValidator).Delete("/roles/{id}/permissions", rr.RoleController.RemovePermissionFromRole)
+	r.Get("/role-permissions", rr.RoleController.GetAllRolePermissions)
+	r.With(middlewares.JWTAuthMiddleware, middlewares.RequireAllRoles("admin")).Post("/roles/{userId}/assign/{roleId}", rr.RoleController.AssignRoleToUser)
+
 }
